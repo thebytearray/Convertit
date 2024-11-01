bf72750a6c5988628347e2073fe02f368b0661d2
Tamim Hossain <132823494+CodeWithTamim@users.noreply.github.com>
2024-11-01 17:57:25 +0600

    up sdk 35
    up sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.nasahacker.convertit"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.nasahacker.convertit"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 6
         versionName = "1.0.5-beta"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
