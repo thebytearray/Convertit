0dbb8c32ffb7ed3aff494a506c6e124f65168012
CodeWithTamim <tamimh.dev@gmail.com>
2024-12-13 23:24:25 +0600

    up 1.1.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nasahacker.convertit"
         minSdk = 21
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.9-beta"
+        versionCode = 11
+        versionName = "1.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
