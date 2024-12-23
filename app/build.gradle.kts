cf285ac00a2025e84bd465ff3c7f8b9f7c69ccd7
Tamim Hossain <132823494+CodeWithTamim@users.noreply.github.com>
2024-12-23 16:05:17 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nasahacker.convertit"
         minSdk = 21
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1.0-beta"
+        versionCode = 12
+        versionName = "1.1.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
