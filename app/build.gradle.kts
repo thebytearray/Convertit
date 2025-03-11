8f52f730883bf97e3d4d87d417474f161ea6396b
Tamim Hossain <132823494+CodeWithTamim@users.noreply.github.com>
2025-03-11 11:44:44 +0600

    Up 1.2.2-lts
    Up 1.2.2-lts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = packageName
         minSdk = 21
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.2.1-lts"
+        versionCode = 23
+        versionName = "1.2.2-lts"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
