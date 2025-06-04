65c8978d36e0ceba4ba6bd01f12ff66dbf99ce50
Tamim Hossain <tamimh.dev@gmail.com>
2025-06-05 02:42:40 +0600

    update gradle

--- app/src/main/java/com/nasahacker/convertit/service/ConvertItService.kt
+++ app/src/main/java/com/nasahacker/convertit/service/ConvertItService.kt
@@ -12,6 +12,7 @@ package com.nasahacker.convertit.service
  */
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
